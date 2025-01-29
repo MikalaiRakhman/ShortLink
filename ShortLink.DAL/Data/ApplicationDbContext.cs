3bf684bd6ffaded166547acf0f473e8570967eb7
MikalaiRakhman <rakhmanmikalai@gmail.com>
2025-01-29 19:46:44 +0100

    New migration. Added Identity.

--- ShortLink.DAL/Data/ApplicationDbContext.cs
+++ ShortLink.DAL/Data/ApplicationDbContext.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ShortLink.DAL.Identity;
 using ShortLink.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace ShortLink.DAL.Data
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext: IdentityDbContext<User, IdentityRole<Guid>, Guid>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
         {
         }
 
-        public DbSet<DoubleUrl> DoubleUrls { get; set; }        
+        public DbSet<DoubleUrl> DoubleUrls { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
