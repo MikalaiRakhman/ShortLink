f76a55193b1d38649b90b26dffdab8f0cc3bc291
MikalaiRakhman <rakhmanmikalai@gmail.com>
2025-01-29 18:42:35 +0100

    Added ShortUrlController. Added two commands: CreateShortUrl, GetOriginalUrl.

---
+++ ShortLink/Controllers/ShortUrlController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using ShortLink.BL.GetOriginalUrl;
+using ShortLink.BL.CreateShortUrl;
+
+namespace ShortLink.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShortUrlController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public ShortUrlController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("get-original-url")]
+        public async Task<IActionResult> GetOriginalUrl([FromBody] GetOriginalUrlCommand command)
+        {
+            var originalUrl = await _mediator.Send(command);
+
+            return Ok(new { originalUrl });
+        }
+
+        [HttpPost("create-short-url")]
+        public async Task<IActionResult> CreateShortUrl([FromBody] CreateDoubleUrlCommand command)
+        {
+            var shortUrl = await _mediator.Send(command);
+            return Ok(new { shortUrl });
+        }
+        
+    }
+}
