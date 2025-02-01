c34b09c4c037fc537b0bd268beab2b2904fa19e6
MikalaiRakhman <rakhmanmikalai@gmail.com>
2025-02-01 13:32:31 +0100

    change email exception messege

--- ShortLink/Models/LoginModel.cs
+++ ShortLink/Models/LoginModel.cs
@@ -5,7 +5,7 @@ namespace ShortLink.Web.Models
     public class LoginModel
     {
         [Required(ErrorMessage = "Email is required.")]
-        [EmailAddress(ErrorMessage = "Email must be a valid email address.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Password is required.")]
         [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
